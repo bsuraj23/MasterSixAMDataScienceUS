ca4f88830df2eb6093eb5090d7cc2f37c6d86393
yogeshwarirj <rj.yogeshwari@gmail.com>
2026-03-05 11:37:20 -0500

    Added introduction.py to branch yogeshwari

---
+++ introduction.py
@@ -0,0 +1,111 @@
+1️. What is Python?
+
+Python is a high-level, interpreted programming language known for its simple syntax and readability. It is widely used in web development, data science, AI, and automation.
+***********************************
+2️. Who created Python?
+
+Python was created by Guido van Rossum in 1989 and released in 1991.
+****************************************
+3.What are the main features of Python?
+
+Easy to learn and read
+
+Interpreted language
+
+Large standard library
+
+Supports multiple programming paradigms
+
+Open-source
+****************************************
+4.What are Python data types?
+
+Common data types include:
+
+int – integers
+
+float – decimal numbers
+
+str – text
+
+list – ordered collection
+
+tuple – immutable collection
+
+dict – key-value pairs
+****************************************
+
+5.What is PEP 8?
+
+PEP 8 is the official style guide for writing Python code, helping developers write clean and readable programs
+
+**********************************
+6.What is the difference between compiled and interpreted languages?
+
+Compiled language → Code is converted to machine code before execution.
+
+Interpreted language → Code runs line by line using an interpreter like in Python.
+*******************************
+6.What does “Interpreted Language” mean in Python?
+
+Python is an interpreted programming language, which means the code is executed line-by-line by an interpreter instead of being compiled all at once.
+
+Python programs are executed using the **CPython interpreter.
+
+🔹 How it works
+
+You write Python code (.py file).
+
+The interpreter converts the code into bytecode.
+
+The bytecode is executed by the Python Virtual Machine (PVM).
+
+🔹 Advantages
+
+Easier debugging (errors appear line by line)
+
+Faster development
+
+No separate compilation step needed
+
+Example:
+
+print("Hello World")
+
+The interpreter reads and executes this line immediately.
+
+ 7.How is Python Portable?
+
+Python is considered portable because the same Python code can run on different operating systems without changing the code.
+
+Example operating systems:
+
+Windows
+
+Linux
+
+macOS
+
+If Python is installed on these systems, the same .py file will run on all of them.
+
+🔹 Example
+
+A program written on Windows:
+
+print("Data Science Project")
+
+The same program can run on Linux or macOS without modification.
+
+🔹 Why Python is Portable
+
+Python interpreter exists for many platforms
+
+Standard libraries work across systems
+
+Code is platform-independent
+
+✅ Short  answer
+
+Interpreted: Python executes code line-by-line using an interpreter.
+
+Portable: Python programs can run on different operating systems like Windows, Linux, and macOS without changing the code.
