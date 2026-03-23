bb095545e2959e16d574ef7fc71b4df98afa50d7
Teena <teenak0287@gmail.com>
2026-03-23 15:58:52 -0700

    Added a Class file

---
+++ teena_assignments/Classes/person.py
@@ -0,0 +1,8 @@
+class Person():
+    def __init__(self,name,age):
+       self.name = name
+       self.age  = age
+    def greet(self):
+        print("Hello my name is " +self.name)
+p1 = Person("John" , 35)
+p1.greet()
